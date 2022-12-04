d24ea1eafb82d4f6523a1bc051fa852dc4266583
Ibrahim Bali <balii@pm.me>
2022-12-05 01:08:44 +0300

    feat: configure lambda backend

--- backend/src/lambda.rs
+++ backend/src/lambda.rs
@@ -1,17 +1,23 @@
-use lambda_runtime::{service_fn, Error, LambdaEvent};
+use crate::api::token::{token_func_async, TokenBody};
+use lambda_http::{service_fn, Error, Request, RequestExt};
 use serde_json::{json, Value};
 
 pub async fn run() -> Result<(), Error> {
-    let func = service_fn(func);
-    lambda_runtime::run(func).await?;
-
-    Ok(())
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .without_time()
+        .init();
+    lambda_http::run(service_fn(function_handler)).await
 }
 
-async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
-    let (event, _context) = event.into_parts();
-    let token_body = serde_json::from_value::<crate::api::token::TokenBody>(event).unwrap();
-    let result = crate::api::token::token_func_async(token_body).await;
+async fn function_handler(event: Request) -> Result<Value, Error> {
+    let token_body = event
+        .payload::<TokenBody>()
+        .expect("Unable to parse Form to Option")
+        .expect("Unable to parse Form to Option");
+    println!("{:?}", token_body);
+    let result = token_func_async(token_body).await;
 
     Ok(json!(result))
 }
